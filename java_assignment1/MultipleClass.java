28b1198322bf24a84b0360d8a2a417fe94e78eb2
Narayanan-005 <142734364+Narayanan-005@users.noreply.github.com>
2024-02-22 10:25:41 +0530

    Add files via upload

---
+++ java_assignment1/MultipleClass.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+class Class1{
+    
+}
+class Class2{
+    
+}
+class Class3{
+    
+}
+class Class4{
+
+}
+class Class5{
+
+}
---
+++ java_assignment1/RandomNum.java
@@ -0,0 +1,6 @@
+class RandomNum {
+    public static void main(String[] args) {
+        int ran=(int)(Math.random()*10);
+        System.out.println(ran);
+    }
+}
